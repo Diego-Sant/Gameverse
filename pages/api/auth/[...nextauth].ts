8c1931ecc4744377a87c992db326614130f75069
Diego de Souza Santana <79605718+Diego-Sant@users.noreply.github.com>
2023-06-12 18:31:12 -0300

    credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,6 @@
 import NextAuth, { AuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import {compare} from "bcrypt";
 
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -10,9 +12,46 @@ export const authOptions: AuthOptions = ({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+        }),
+        Credentials({
+            id: 'credentials',
+            name: 'Credentials',
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'Senha',
+                    type: 'password'
+                }
+            },
+            async authorize(credentials) {
+
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Email e senha são obrigatórios!");
+                }
+
+                const user = await prismadb.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user || !user.hashedPassword) {
+                    throw new Error('Desculpe, não encontramos uma conta com esse endereço de email.')
+                }
+
+                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);
+
+                if (!isCorrectPassword) {
+                    throw new Error("Senha incorreta!")
+                }
+
+                return user;
+            }
         })
     ],
-    
     pages: {
         signIn: '/login',
     },
